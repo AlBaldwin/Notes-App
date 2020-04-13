68f6b103d7adb450a5558b2692566dc216ced59a
Alan Baldwin <baldwingaming1979@gmail.com>
2020-04-13 09:51:44 +0100

    Added core app functionality

---
+++ app.js
@@ -0,0 +1,71 @@
+const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes')
+
+
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removing the note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOptions: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing the notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+
+yargs.parse()
